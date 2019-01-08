a4716e0162c00ba173fc97c7590e3b5866651532
peidongGuo <gpd_wfe@163.com>
2019-01-08 14:49:53 +0800

    finish formik&antd&yup form

--- src/pages/NormalLoginForm/NormalLoginForm.tsx
+++ src/pages/NormalLoginForm/NormalLoginForm.tsx
@@ -2,60 +2,81 @@ import * as React from 'react';
 import 'antd/dist/antd.css';
 import './NormalLoginForm.css';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
-interface Props {
-  form: any;
-}
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+interface Props {}
 
 interface State {}
-class LoginForm extends React.Component<Props, State> {
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-  };
-
+class NormalLoginForm extends React.Component<Props, State> {
   render() {
-    const { getFieldDecorator } = this.props.form;
+    const SignupSchema = Yup.object().shape({
+      username: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+      password: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required')
+    });
     return (
       <div className="form-wrapper">
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <Form.Item>
-            {getFieldDecorator('userName', {
-              rules: [{ required: true, message: 'Please input your username!' }]
-            })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />)}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Please input your Password!' }]
-            })(
-              <Input
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="password"
-                placeholder="Password"
-              />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('remember', {
-              valuePropName: 'checked',
-              initialValue: true
-            })(<Checkbox>Remember me</Checkbox>)}
-            <a className="login-form-forgot" href="">
-              Forgot password
-            </a>
-            <Button type="primary" htmlType="submit" className="login-form-button">
-              Log in
-            </Button>
-            Or <a href="">register now!</a>
-          </Form.Item>
-        </Form>
+        <Formik
+          initialValues={{
+            username: '',
+            password: ''
+          }}
+          validationSchema={SignupSchema}
+          onSubmit={(values) => {
+            // same shape as initial values
+            console.log(values);
+          }}
+        >
+          {({ values, errors, handleBlur, handleChange, handleSubmit }) => (
+            <Form onSubmit={handleSubmit} className="login-form">
+              <Form.Item
+                validateStatus={errors && errors.username ? 'error' : ''}
+                help={errors && errors.username ? errors.username : ''}
+              >
+                <Input
+                  name="username"
+                  value={values.username}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  placeholder="Username"
+                />
+              </Form.Item>
+              <Form.Item
+                validateStatus={errors && errors.password ? 'error' : ''}
+                help={errors && errors.password ? errors.password : ''}
+              >
+                <Input
+                  name="password"
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  type="password"
+                  placeholder="Password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Form.Item>
+              <Form.Item>
+                <Checkbox>Remember me</Checkbox>
+                <a className="login-form-forgot" href="">
+                  Forgot password
+                </a>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                  Log in
+                </Button>
+                Or <a href="">register now!</a>
+              </Form.Item>
+            </Form>
+          )}
+        </Formik>
       </div>
     );
   }
 }
-const NormalLoginForm = Form.create()(LoginForm);
 
 export default NormalLoginForm;
